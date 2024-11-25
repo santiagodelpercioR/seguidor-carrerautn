b1a7638798649839ee184fae708a3ea70139df4f
santiagodelpercioR <santiagodelpercio@gmail.com>
2024-11-24 22:22:21 -0300

    separacion de anios e implementacion de nuevas materias, disenio mejorado aplicando flexbox

--- script.js
+++ script.js
@@ -99,6 +99,19 @@ const materias = [
     new Materia(3,21,"Comunicación de Datos", {cursadas: [], aprobadas: [3,7]}),
     new Materia(3,22,"Análisis Numérico", {cursadas: [9], aprobadas: [1,2]}),
     new Materia(3,23,"Diseño de Sistemas de Información", {cursadas: [14,16], aprobadas: [4,6,8]}),
+    new Materia(4,24,"Legislación", {cursadas: [11]}),
+    new Materia(4,25,"Ingeniería y Calidad de Software", {cursadas: [19,20,23], aprobadas:[13,14]}),
+    new Materia(4,26,"Redes de Datos", {cursadas: [15,21]}),
+    new Materia(4,27,"Investigación Operativa", {cursadas: [17,22]}),
+    new Materia(4,28,"Simulación", {cursadas: [17], aprobadas:[9]}),
+    new Materia(4,29,"Tecnologías para la automatización", {cursadas: [10,22], aprobadas:[9]}),
+    new Materia(4,30,"Administración de Sistemas de Información", {cursadas: [18,23], aprobadas:[16]}),
+    new Materia(5,31,"Inteligencia Artificial", {cursadas: [28], aprobadas:[17,22]}),
+    new Materia(5,32,"Ciencia de Datos", {cursadas: [28], aprobadas:[17,19]}),
+    new Materia(5,33,"Sistemas de Gestión", {cursadas: [18,27], aprobadas:[23]}),
+    new Materia(5,34,"Gestión Gerencial", {cursadas: [24,30], aprobadas:[18]}),
+    new Materia(5,35,"Seguridad en los Sistemas de Información", {cursadas: [26,30], aprobadas:[20,21]}),
+    new Materia(5,36,"Proyecto Final", {cursadas: [25,26,30], aprobadas:[12,20,23]}),
 ];
 
 const materiasRegularizadas = [];
